b0b343650ca032a17ba4709394a671496cd4de53
clintonjosephs <clintonjosephs26@gmail.com>
2022-05-16 13:04:14 +0100

    finalize design definitions for the project

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,3 @@
 class ApplicationController < ActionController::Base
-    add_flash_types :danger, :info, :warning, :success, :messages, :notice, :alert
+  add_flash_types :danger, :info, :warning, :success, :messages, :notice, :alert
 end
