48283be9a649a231556ad180442564e550e68f9c
clintonjosephs <clintonjosephs26@gmail.com>
2022-05-17 11:11:36 +0100

    implemented design of the add food form

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,3 +1,8 @@
 class FoodsController < ApplicationController
   def index; end
+
+  def new
+    @food = Food.new
+    @measurement_units = [['grams', 'g'], ['kilograms', 'kg'], ['milligrams', 'ml'], ['liter', 'l'], ['ounces', 'oz'], ['pound', 'lb'], ['cup', 'cup'], ['pint', 'pint'], ['quart', 'quart'], ['gallon', 'gallon']]
+  end
 end
--- app/models/food.rb
+++ app/models/food.rb
@@ -1,4 +1,9 @@
 class Food < ApplicationRecord
   belongs_to :user
   has_many :inventory_foods, dependent: :destroy
+  has_one_attached :image
+
+  validates :name, presence: true, length: { maximum: 100 }
+  validates :measurement_unit, presence: true
+  validates :price, presence: true, numericality: { greater_than: 0 }
 end
