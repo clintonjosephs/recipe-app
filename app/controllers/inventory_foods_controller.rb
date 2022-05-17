1ab578c1eb93f94c6932e51b1d3caabccb00b242
clintonjosephs <clintonjosephs26@gmail.com>
2022-05-17 16:36:37 +0100

    setup new action in inventoryfoods controller

--- app/controllers/inventory_foods_controller.rb
+++ app/controllers/inventory_foods_controller.rb
@@ -1,4 +1,9 @@
 class InventoryFoodsController < ApplicationController
     load_and_authorize_resource
     before_action :authenticate_user!
+
+    def new
+        @inventory_food = InventoryFood.new
+        @foods = Food.all.where(user_id: current_user.id).order(name: :asc)
+    end
 end
