fdf36f35b70d6294f2a7a59dfb33af4246ab577d
Samuel Mwape <43408618+Mwapsam@users.noreply.github.com>
2022-05-18 15:52:42 +0200

    Complete public recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,3 +1,7 @@
 class RecipesController < ApplicationController
   def index; end
+
+  def public_recipes
+    @recipes = Recipe.includes(:recipe_foods).where(public: true).order('created_at DESC')
+  end
 end
