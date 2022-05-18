83c11d0da49ee8290f16f7fcd18fe459b84a16ea
clintonjosephs <clintonjosephs26@gmail.com>
2022-05-18 21:19:15 +0100

    fixed linters errors

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -17,15 +17,17 @@ class FoodsController < ApplicationController
     # get all recipe foods
     # get all recipe foods that are not in inventory
     # get all recipe foods that the quantity is higher than the inventory quantity
-    # a shopping list is made up of a combination of items that do not exist in the inventory and items that are not enough in the inventory
+    # a shopping list is made up of a combination of items
+    # that do not exist in the inventory and items that are not enough in the inventory
 
     @recipe_foods = Food.all.joins('INNER JOIN recipe_foods ON foods.id = recipe_foods.food_id')
       .order(created_at: :desc).select('foods.*, recipe_foods.quantity, recipe_foods.id as recipe_foods_id')
       .where(recipe_foods: { recipe_id: params[:recipe_id] })
-    
-    @inventory_foods = Food.all.joins('INNER JOIN inventory_foods ON foods.id = inventory_foods.food_id').select('foods.*, inventory_foods.quantity, inventory_foods.id as inventory_foods_id')
-                        .where(inventory_foods: { inventory_id: params[:inventory_id] })
-    
+
+    @inventory_foods = Food.all.joins('INNER JOIN inventory_foods ON foods.id = inventory_foods.food_id')
+      .select('foods.*, inventory_foods.quantity, inventory_foods.id as inventory_foods_id')
+      .where(inventory_foods: { inventory_id: params[:inventory_id] })
+
     @recipe_food_with_quantity_greater_than_inventory_food_quantity = []
     @recipe_foods.select do |recipe_food|
       @inventory_foods.select do |inventory_food|
@@ -35,16 +37,15 @@ class FoodsController < ApplicationController
         end
       end
     end
-    
+
     @recipe_foods_not_in_inventory = @recipe_foods - @inventory_foods
 
     @shopping_list = @recipe_foods_not_in_inventory + @recipe_food_with_quantity_greater_than_inventory_food_quantity
 
     @cost = 0
-    @shopping_list.each do |list|  
-        @cost += list.price * list.quantity
-    end 
-
+    @shopping_list.each do |list|
+      @cost += list.price * list.quantity
+    end
   end
 
   def create
