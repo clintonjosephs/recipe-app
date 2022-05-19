5cd84eb21fbceb95f194a9d5a9ea275ca815530d
Samuel Mwape <43408618+Mwapsam@users.noreply.github.com>
2022-05-19 16:26:52 +0200

    Test for food model price validation

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -38,6 +38,11 @@ RSpec.describe Food, type: :model do
             expect(@food).to_not be_valid
         end
 
+        it 'should not be valid without price' do
+            @food.price = nil
+            expect(@food).to_not be_valid
+        end
+
         
     end
 end
