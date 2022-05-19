234a70feed5d75dde1fd70bb5a8f68f766455f33
Samuel Mwape <43408618+Mwapsam@users.noreply.github.com>
2022-05-19 16:33:22 +0200

    Validate correct attributes in inventory food

---
+++ spec/models/inventory_food_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+RSpec.describe InventoryFood, type: :model do
+    describe 'Validation for inventory_food model' do
+        before(:each) do
+            @user = User.create(
+                email: 'test@mail.com',
+                password: '12345678',
+                password_confirmation: '12345678'
+            )
+            @inventory = Inventory.create(
+                name: 'Inventory',
+                description: 'Inventory',
+                user_id: @user.id
+            )
+            @food = Food.create(
+                name: 'Food',
+                measurement_unit: 'g',
+                price: '10',
+                user_id: @user.id
+            )
+            @inventory_food = InventoryFood.new(
+                inventory_id: @inventory.id,
+                food_id: @food.id,
+                quantity: '10'
+            )
+
+        end
+
+        before {@inventory_food.save}
+
+        it 'should be valid with valid attributes' do
+            @inventory_food.inventory_id = @inventory.id
+            @inventory_food.food_id = @food.id
+            @inventory_food.quantity = '10'
+            expect(@inventory_food).to be_valid
+        end
+
+        
+    end
+end
