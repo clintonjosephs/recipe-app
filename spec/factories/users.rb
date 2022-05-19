7ee8c355d37b013d97c03b3c7a7ed17f97bbdc99
clintonjosephs <clintonjosephs26@gmail.com>
2022-05-19 15:27:22 +0100

    wrote feature tests for publics and signin features

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     name { Faker::Name.name }
     email { Faker::Internet.email }
-    password { Faker::Internet.password }
+    password { 'secret' }
     password_confirmation { password }
     confirmed_at { Time.now }
   end
