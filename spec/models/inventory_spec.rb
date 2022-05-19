cb6e13748ce505e5175db070aa9a7ace4d514e5e
Samuel Mwape <43408618+Mwapsam@users.noreply.github.com>
2022-05-19 16:41:33 +0200

    Validate correct attribute invontory model

---
+++ spec/models/inventory_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe Inventory, type: :model do
+    describe 'Validation for inventory model' do
+        before(:each) do
+            @user = User.create(
+                email: 'test@mail.com',
+                password: '12345678',
+                password_confirmation: '12345678'
+            )
+
+            @inventory = Inventory.new(
+                name: 'Inventory',
+                description: 'Inventory',
+                user_id: @user.id
+            )
+
+        end
+
+        before {@inventory.save}
+
+        it 'should be valid with valid attributes' do
+            @inventory.id = 1
+            @inventory.name = 'Inventory'
+            @inventory.description = 'Inventory'
+            @inventory.user_id = @user.id
+            expect(@inventory).to be_valid
+        end
+
+        
+    end
+end
