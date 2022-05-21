ff930d86ad807dd380bc782265717c0577b4d0bd
clintonjosephs <clintonjosephs26@gmail.com>
2022-05-21 16:18:00 +0100

    made changes to letter_opener configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'https://peaceful-plateau-96727.herokuapp.com/', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'https://peaceful-plateau-96727.herokuapp.com/' }
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
 end
