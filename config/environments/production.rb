f8ad00da3f57016173af0f364f3ac13de383822f
clintonjosephs <clintonjosephs26@gmail.com>
2022-05-21 17:55:09 +0100

    added closing to confirmation message

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'gmail.com' }
+  config.action_mailer.default_url_options = { host: 'https://peaceful-plateau-96727.herokuapp.com/' }
   config.action_mailer.smtp_settings = {
       :address              => "smtp.gmail.com",
       :port                 => 587,
